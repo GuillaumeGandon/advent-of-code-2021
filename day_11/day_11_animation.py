dfafcd045a551b0b06069a3b4fb865ca914bed84
Guillaume Gandon <guillaume.gandon3@gmail.com>
2021-12-11 12:30:56 +0100

    Update day_11_animation.py

--- day_11/day_11_animation.py
+++ day_11/day_11_animation.py
@@ -1,4 +1,33 @@
+from os import system
+from time import sleep
+
+system("cls")
+
 ADJACENTS = ((-1 - 1j), (0 - 1j), (1 - 1j), (-1 + 0j), (1 + 0j), (-1 + 1j), (0 + 1j), (1 + 1j))
+
+
+def print_animation(step, flashes, part_one):
+    a = (
+        ["\033c"]
+        + list(
+            "    "
+            + "".join(
+                str(octopuses[x + 1j * y] if octopuses[x + 1j * y] != 0 else f"\033[92m{octopuses[x + 1j * y]}\033[0m")
+                for x in range(len(input[0]))
+            )
+            for y in range(len(input))
+        )
+        + ["\n"]
+    )
+    a[1] += f"    \033[93mstep: {step}\033[0m"
+    a[3] += f"    \033[93mtotal flashes: {flashes}\033[0m"
+    a[5] += f"    \033[96manswer part one: {part_one if part_one else ''}\033[0m"
+    a[7] += f"    \033[36manswer part two: {step if all(v == 0 for v in octopuses.values()) else '' }\033[0m"
+    print("\n".join(a))
+    if step < 20:
+        sleep(1 - 0.9 * (step - 1) / 20)
+    else:
+        sleep(0.1)
 
 
 def compute_step():
@@ -26,13 +55,14 @@ def compute_step():
 def compute():
     flashes = 0
     step = 1
+    part_one = None
     while True:
         compute_step()
         flashes += sum(1 for v in octopuses.values() if v == 0)
+        print_animation(step, flashes, part_one)
         if step == 100:
-            print(f"Answer part one: {flashes}")
+            part_one = flashes
         if all(v == 0 for v in octopuses.values()):
-            print(f"Answer part two: {step}")
             break
         step += 1
 
